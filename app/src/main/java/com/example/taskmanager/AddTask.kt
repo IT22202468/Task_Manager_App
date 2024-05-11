4cbe72734a1078d7955d2e4938737695e07305a7
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-05-12 04:32:12 +0530

    Insertion is working.

--- app/src/main/java/com/example/taskmanager/AddTask.kt
+++ app/src/main/java/com/example/taskmanager/AddTask.kt
@@ -2,10 +2,28 @@ package com.example.taskmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.taskmanager.databinding.ActivityAddTaskBinding
 
 class AddTask : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+    private lateinit var db: TasksDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TasksDatabaseHelper(this)
+
+        binding.saveBtn.setOnClickListener {
+            val title = binding.titleEdit.text.toString()
+            val content = binding.contentEdit.text.toString()
+            val task = Task(0, title, content)
+            db.insertTask(task)
+            finish()
+            Toast.makeText(this, "Task saved", Toast.LENGTH_SHORT).show()
+        }
     }
 }
